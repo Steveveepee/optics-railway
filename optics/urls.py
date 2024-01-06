ff914e8a775bc14b8a36114539f53ced0d0d93e3
Jason van Lint <jvanlint@mac.com>
2024-01-06 23:56:56 +1100

    Add coming_soon view to urlpatterns

--- optics/urls.py
+++ optics/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
+from django.urls import include
 from django.contrib import admin
 from django.urls import path
 from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+]
+
+urlpatterns += [
     path("", views.coming_soon, name="coming_soon"),
 ]
