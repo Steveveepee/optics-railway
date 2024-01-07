1661a32a55dacb9b37bbe0282f18262d1991bc56
Jason van Lint <jvanlint@mac.com>
2024-01-07 16:56:07 +1100

    Delete unnecessary files and update app references

--- optics/urls.py
+++ optics/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import RedirectView
 from django.urls import include
 from django.contrib import admin
 from django.urls import path
-from core import views
+from optics.core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
